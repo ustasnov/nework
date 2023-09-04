b931a691cf90d0d9928b5397c04169f2a0c39be8
ustasnov <ustasnov@gmail.com>
2023-09-04 23:20:33 +0700

    Change CI

--- app/src/main/java/ru/netology/nmedia/AppActivity.kt
+++ app/src/main/java/ru/netology/nmedia/AppActivity.kt
@@ -1,22 +1,15 @@
 package ru.netology.nmedia
 
-import android.Manifest
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.os.Build
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
-import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.MenuProvider
 import androidx.navigation.findNavController
-import com.google.android.gms.common.ConnectionResult
-import com.google.android.gms.common.GoogleApiAvailability
 import com.google.android.material.snackbar.Snackbar
-import com.google.firebase.messaging.FirebaseMessaging
 import dagger.hilt.android.AndroidEntryPoint
 import ru.netology.nmedia.NewPostFragment.Companion.isNewPost
 import ru.netology.nmedia.NewPostFragment.Companion.textArg
