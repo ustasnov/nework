868278b84820952653b866e765de7842173941b8
ustasnov <ustasnov@gmail.com>
2023-10-31 17:19:59 +0700

    Changes for ApiService

--- app/src/main/java/ru/netology/nmedia/api/ApiService.kt
+++ app/src/main/java/ru/netology/nmedia/api/ApiService.kt
@@ -4,6 +4,7 @@ import okhttp3.MultipartBody
 import retrofit2.Response
 import retrofit2.http.*
 import ru.netology.nmedia.dto.Event
+import ru.netology.nmedia.dto.Job
 import ru.netology.nmedia.dto.Media
 import ru.netology.nmedia.dto.Post
 import ru.netology.nmedia.dto.User
@@ -81,5 +82,53 @@ interface ApiService {
 
     @GET("events/{id}/newer")
     suspend fun getNewerEvents(@Path("id") id: Long): Response<List<Event>>
+
+    // wall
+
+    @GET("{author_id}/wall/")
+    suspend fun getAllWallPosts(@Path("author_id") authorId: Long): Response<List<Post>>
+
+    @GET("{author_id}/wall/latest")
+    suspend fun getLatestWallPosts(@Path("author_id") authorId: Long, @Query("count") count: Int): Response<List<Post>>
+
+    @GET("{author_id}/wall/{post_id}/before")
+    suspend fun getBeforeWallPosts(@Path("author_id") authorId: Long, @Path("post_id") postId: Long, @Query("count") count: Int): Response<List<Post>>
+
+    @GET("{author_id}/wall/{post_id}/after")
+    suspend fun getAfterWallPosts(@Path("author_id") authorId: Long, @Path("post_id") postId: Long, @Query("count") count: Int): Response<List<Post>>
+
+    @GET("{author_id}/wall/{post_id}/newer")
+    suspend fun getNewerWallPosts(@Path("author_id") authorId: Long, @Path("post_id") postId: Long, @Query("count") count: Int): Response<List<Post>>
+
+    // my wall
+
+    @GET("my/wall")
+    suspend fun getAllMyWallPosts(): Response<List<Post>>
+
+    @GET("my/wall/latest")
+    suspend fun getLatestMyWallPosts(@Query("count") count: Int): Response<List<Post>>
+
+    @GET("my/wall/{post_id}/before")
+    suspend fun getBeforeMyWallPosts(@Path("post_id") postId: Long, @Query("count") count: Int): Response<List<Post>>
+
+    @GET("my/wall/{post_id}/after")
+    suspend fun getAfterMyWallPosts(@Path("post_id") postId: Long, @Query("count") count: Int): Response<List<Post>>
+
+    @GET("my/wall/{post_id}/newer")
+    suspend fun getNewerMyWallPosts(@Path("post_id") postId: Long): Response<List<Post>>
+
+    // jobs
+
+    @GET("my/jobs")
+    suspend fun getAllMyJobs(): Response<List<Job>>
+
+    @POST("my/jobs")
+    suspend fun saveMyJob(@Body job: Job): Response<Job>
+
+    @DELETE("my/jobs/{job_id}")
+    suspend fun removeByIdMyJob(@Path("job_id") jobId: Long): Response<Unit>
+
+    @GET("{user_id}/jobs")
+    suspend fun getAllUserJobs(@Path("user_id") userId: Long): Response<List<Job>>
 
 }
---
+++ app/src/main/java/ru/netology/nmedia/dto/Job.kt
@@ -0,0 +1,10 @@
+package ru.netology.nmedia.dto
+
+data class Job(
+    override val id: Long,
+    val name: String,
+    val position: String,
+    val start: String,
+    val finish: String?,
+    val link: String?
+): FeedItem
